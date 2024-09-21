c99bc74ad503d96b0cbc529c29dfbe767dc9e616
HappY Millionaire <sunnat.fakhreev.02@gmail.com>
2024-09-21 20:56:55 +0300

    Переменные исправленные ошибки

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 name = 'Python'
 firs_name = 'Python,'
-total_task = '12,'
-hours_spent = '1.5,'
-average_execution_time = '0.125 часа'
-print(firs_name, 'всего задач:',total_task , 'затрачено часов:', hours_spent,'среднее время выполнения', average_execution_time)
+total_task = 12
+hours_spent = 1.5
+average_execution_time = (1.5 / 12)
+print (average_execution_time)
+print(firs_name, 'всего задач:',total_task,',', 'затрачено часов:',hours_spent,',', 'среднее время выполнения', average_execution_time)
