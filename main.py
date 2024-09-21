a3ea713567f9451b3e16bc754d0edbe23252c009
HappY Millionaire <sunnat.fakhreev.02@gmail.com>
2024-09-21 16:39:49 +0300

    Переменные

---
+++ main.py
@@ -0,0 +1,6 @@
+name = 'Python'
+firs_name = 'Python,'
+total_task = '12,'
+hours_spent = '1.5,'
+average_execution_time = '0.125 часа'
+print(firs_name, 'всего задач:',total_task , 'затрачено часов:', hours_spent,'среднее время выполнения', average_execution_time)
